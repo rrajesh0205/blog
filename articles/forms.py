f0269a04ea3439cf0fef5d346918f06c0fc36300
rrajesh0205 <rajesh.ramamoorthy@outlook.in>
2023-03-05 18:12:47 +0530

    Create menu is now modified.

--- articles/forms.py
+++ articles/forms.py
@@ -7,4 +7,4 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ('title', 'body', 'img')
-        labels = {'title': 'Blog Title', 'body': 'Content of the Blog', 'img': 'Any Image related to the Content'}
+        labels = {'title': 'lets give a title', 'body': 'write what you want to', 'img': 'want to upload image...'}
